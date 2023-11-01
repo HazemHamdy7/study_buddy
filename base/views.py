c0e2db70ce4175d9e61504dce8c00a17b202a066
HazemHamdy7 <HazemHamdy7.com>
2023-11-01 22:50:55 +0200

    create base project andd add templates

--- base/views.py
+++ base/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Lets Learn Python!'},
+    {'id': 2, 'name': 'Design with me !'},
+    {'id': 3, 'name': 'Frondend developers!'},
+]
+
+
+def home(requset):
+    return render(requset, 'home.html', {'rooms': rooms})
+
+
+def room(requset):
+    return render(requset, 'room.html')
