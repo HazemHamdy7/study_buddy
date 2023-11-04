543f1ddeed441885d1d62e34ccf32dc6f165c151
HazemHamdy7 <HazemHamdy7.com>
2023-11-04 12:14:52 +0200

    add send message in Conversation

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.http import HttpResponse
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from . forms import RoomForm
 
 # Create your views here.
@@ -91,7 +91,20 @@ def room(request, pk):
     # context = {'room': room}
 
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+    context = {
+        'room': room,
+        'room_messages': room_messages,
+    }
     return render(request, 'base/room.html', context)
 
 
