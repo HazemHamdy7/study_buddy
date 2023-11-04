50991c85a620cb975f3c6a4c68ae40b0cb0d2efe
HazemHamdy7 <HazemHamdy7.com>
2023-11-04 12:28:50 +0200

    show user in  participations bar

--- base/views.py
+++ base/views.py
@@ -92,6 +92,7 @@ def room(request, pk):
 
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()
+    participations = room.participations.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -99,11 +100,13 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participations.add(request.user)
         return redirect('room', pk=room.id)
 
     context = {
         'room': room,
         'room_messages': room_messages,
+        'participations': participations,
     }
     return render(request, 'base/room.html', context)
 
