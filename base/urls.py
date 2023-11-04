a7c7d70a11ae05d663dbd87baa8644e4fa33c41e
HazemHamdy7 <HazemHamdy7.com>
2023-11-04 20:35:49 +0200

    add user profile in project

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
 
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
