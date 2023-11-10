a9c6bd1053f4556979c41d8f003436e8efb6c116
HazemHamdy7 <HazemHamdy7.com>
2023-11-10 03:20:23 +0200

    updateUser section and fixed templats

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.db.models import Q
 from .models import Room, Topic, Message
-from . forms import RoomForm
+from . forms import RoomForm, UserForm
 
 # Create your views here.
 
@@ -209,3 +209,18 @@ def deleteMessage(request, pk):
         return redirect('home')
     context = {'obj': message}
     return render(request, 'base/delete.html', context)
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form': form}
+    return render(request, 'base/update_user.html', context)
